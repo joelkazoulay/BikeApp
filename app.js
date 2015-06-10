db10cee8fadd7d5b63cc7796d5a6706cdd554339
Alex Uribe <kenzanmedia@Alexs-MacBook-Pro.local>
2015-06-10 11:25:35 -0600

    Week 2 Commit.  Displaying data objects and using filters.  Installed bootstrap.

--- app.js
+++ app.js
@@ -6,8 +6,27 @@ angular
 function AddBikeCtrl(){
     var mod = this;
     mod.data = null;
-    mod.bikes = ['Justin\'s Bike', 'Craig\'s Bike',
-        'Bill\'s amazing Bike','David\'s awesome bike'];
+    mod.bikes = [
+        {
+            Name: 'Trek',
+            Owner: 'David',
+            Price: 15000,
+            Date: '1922-02-12T02:12:33'
+        },
+        {
+            Name: 'Motorcycle',
+            Owner: 'Justin',
+            Price: 30000,
+            Date: '2010-03-22'
+        },
+        {
+            Name: 'SpecializZed(tm)',
+            Owner: 'Nate',
+            Price: 100,
+            Date: '2014-04-21'
+        }
+    ];
+
 
     mod.addBike = function(data){
         mod.bikes.push(data);
